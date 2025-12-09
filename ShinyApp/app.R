9cc92f25c8753e176c7b11fff844b36b539080be
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2025-12-09 10:29:25 +0100

    some additions

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,38 +1,93 @@
 library(shiny)
 library(bslib)
 
-# See above for the definitions of ui and server
-ui <- page_sidebar(
-  # App title ----
-  title = "Hello Shiny!",
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
+# -------------------------------
+# User Interface
+# -------------------------------
+ui <- page_navbar(
+  title = "Étude des modèles de dégradation",
+  
+  # ---- PAGE 1 ----
+  nav_panel(
+    "Processus Wiener",
+    page_sidebar(
+      sidebar = sidebar(
+        selectInput(
+          inputId = "model_choice",
+          label = "La loi pour les dates de simulation :",
+          choices = c("Constant", "Exponentiel", "Weibull")
+        ),
+        
+        numericInput("pt_nbr", "Nombre points :", 5, min = -100, max = 100),
+        numericInput("moyenne", "Moyenne :", 2, min = 0, max = 100),
+        numericInput("variance", "Variance :", 1),
+        numericInput("traj_nbr", "Nb trajectoires :", 1),
+        numericInput("seuil", "Seuil :", 1)
+      ),
+      plotOutput("plot1")
     )
   ),
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+  
+  # ---- PAGE 2 ----
+  nav_panel(
+    "Processus Gamma",
+    page_sidebar(
+      sidebar = sidebar(
+        numericInput("lambda", "Lambda :", 1)
+      ),
+      plotOutput("plot2")
+    )
+  ),
+  
+  # ---- PAGE 3 ----
+  nav_panel(
+    "Laser Data",
+    page_sidebar(
+      sidebar = sidebar(
+        numericInput("shape", "Shape :", 2),
+        numericInput("scale", "Scale :", 1)
+      ),
+      plotOutput("plot3")
+    )
+  )
 )
 
-server <- function(input, output) {
+# -------------------------------
+# SERVER
+# -------------------------------
+server <- function(input, output, session) {
   
-  output$distPlot <- renderPlot({
+  # ----- PAGE 1 -----
+  output$plot1 <- renderPlot({
+    model <- input$model_choice
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    if (model == "Normal") {
+      x <- rnorm(1000, mean = input$mean, sd = sqrt(input$variance))
+    } else if (model == "Exponentiel") {
+      x <- rexp(1000, rate = 1/input$mean)
+    } else if (model == "Weibull") {
+      x <- rweibull(1000, shape = input$param1, scale = input$param2)
+    }
     
-    hist(x, breaks = bins, col = "#007bc2", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
+    hist(x, main = paste("Distribution:", model), xlab = "", col = "#007bc2", border = "white")
   })
   
+  # ----- PAGE 2 -----
+  output$plot2 <- renderPlot({
+    x <- rexp(500, rate = input$lambda)
+    hist(x, col = "#007bc2", border = "white",
+         main = "Modèle 2")
+  })
+  
+  # ----- PAGE 3 -----
+  output$plot3 <- renderPlot({
+    x <- rweibull(500, shape = input$shape, scale = input$scale)
+    hist(x, col = "#007bc2", border = "white",
+         main = "Modèle 3")
+  })
 }
 
-shinyApp(ui = ui, server = server)
+# -------------------------------
+# Run the app
+# -------------------------------
+shinyApp(ui, server)
