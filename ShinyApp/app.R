69d718f1f91e438c5982092e07120593d92c08bc
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2025-12-09 12:21:10 +0100

    Fully functional Wiener Simulation

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -18,10 +18,10 @@ ui <- page_navbar(
           choices = c("Constant")
         ),
         
-        numericInput("pt_nbr", "Nombre points :", 5, min = -100, max = 100),
-        numericInput("moyenne", "Moyenne :", 2, min = 0, max = 100),
-        numericInput("variance", "Variance :", 1),
-        numericInput("traj_nbr", "Nb trajectoires :", 1),
+        numericInput("pt_nbr", "Nombre points :", 100, min = -100, max = 100),
+        numericInput("moyenne", "Moyenne :", 10, min = 0, max = 100),
+        numericInput("variance", "Variance :", 10),
+        numericInput("traj_nbr", "Nb trajectoires :", 7),
         numericInput("seuil", "Seuil :", 1)
       ),
       plotOutput("plot1")
@@ -61,23 +61,34 @@ server <- function(input, output, session) {
   output$plot1 <- renderPlot({
     mu = input$moyenne
     sigma = sqrt(input$variance)
+    traj_nbr = input$traj_nbr
     L = input$pt_nbr
     T = 10
     N=1000
     t = seq(0,T,length.out = L)
-    Z_k = rnorm(N)
-    B = rep(0,L)
-    for (i in 1:L){
-      u = t[i]
-      s <- 0
-      for(j in 1:N){
-        e_ju = sqrt(8*T)/((2*j+1)*pi)*sin(((2*j+1)*pi*u)/(2*T))
-        s <- s+ Z_k[j]*e_ju
+    
+    simulate = function() {
+      Z_k = rnorm(N)
+      B = rep(0,L)
+      for (i in 1:L){
+        u = t[i]
+        s <- 0
+        for(j in 1:N){
+          e_ju = sqrt(8*T)/((2*j+1)*pi)*sin(((2*j+1)*pi*u)/(2*T))
+          s <- s+ Z_k[j]*e_ju
+        }
+        B[i] = s
       }
-      B[i] = s
+      X = mu*t + sigma*B
+      return(X)
     }
-    X = mu*t + sigma*B
-    plot(t,X,type= 'l')
+    
+    results = replicate(traj_nbr, simulate())
+    plot(t, results[, 1],type= 'l')
+    for (i in 2:ncol(results)) {
+      lines(t, results[, i], type='l')
+    }
+    abline(0, mu, col='red')
   })
   
 }
