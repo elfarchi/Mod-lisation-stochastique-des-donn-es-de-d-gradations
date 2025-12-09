32c7d58ca7191f9958d3cfea012b12389e569727
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2025-12-09 12:21:10 +0100

    Fully functional Wiener Simulation

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -18,10 +18,10 @@ ui <- page_navbar(
           choices = c("Constant", "Exponentiel", "Weibull")
         ),
         
-        numericInput("pt_nbr", "Nombre points :", 5, min = -100, max = 100),
-        numericInput("moyenne", "Moyenne :", 2, min = 0, max = 100),
-        numericInput("variance", "Variance :", 1),
-        numericInput("traj_nbr", "Nb trajectoires :", 1),
+        numericInput("pt_nbr", "Nombre points :", 100, min = -100, max = 100),
+        numericInput("moyenne", "Moyenne :", 10, min = 0, max = 100),
+        numericInput("variance", "Variance :", 10),
+        numericInput("traj_nbr", "Nb trajectoires :", 7),
         numericInput("seuil", "Seuil :", 1)
       ),
       plotOutput("plot1")
@@ -59,6 +59,7 @@ server <- function(input, output, session) {
   
   # ----- PAGE 1 -----
   output$plot1 <- renderPlot({
+<<<<<<< HEAD
     model <- input$model_choice
     
     if (model == "Normal") {
@@ -70,6 +71,38 @@ server <- function(input, output, session) {
     }
     
     hist(x, main = paste("Distribution:", model), xlab = "", col = "#007bc2", border = "white")
+=======
+    mu = input$moyenne
+    sigma = sqrt(input$variance)
+    traj_nbr = input$traj_nbr
+    L = input$pt_nbr
+    T = 10
+    N=1000
+    t = seq(0,T,length.out = L)
+    
+    simulate = function() {
+      Z_k = rnorm(N)
+      B = rep(0,L)
+      for (i in 1:L){
+        u = t[i]
+        s <- 0
+        for(j in 1:N){
+          e_ju = sqrt(8*T)/((2*j+1)*pi)*sin(((2*j+1)*pi*u)/(2*T))
+          s <- s+ Z_k[j]*e_ju
+        }
+        B[i] = s
+      }
+      X = mu*t + sigma*B
+      return(X)
+    }
+    
+    results = replicate(traj_nbr, simulate())
+    plot(t, results[, 1],type= 'l')
+    for (i in 2:ncol(results)) {
+      lines(t, results[, i], type='l')
+    }
+    abline(0, mu, col='red')
+>>>>>>> 69d718f (Fully functional Wiener Simulation)
   })
   
   # ----- PAGE 2 -----
